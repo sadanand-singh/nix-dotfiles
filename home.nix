394f19845521c69c7b24c08e33d7d9e612426bf7
Sadanand Singh <sadanand4singh@gmail.com>
2023-07-02 18:45:10 +0530

    add vscode nix file

--- home.nix
+++ home.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }: {
+  imports = [
+    ./vscode.nix
+  ];
   home.username = "sadanand"; # REPLACE ME
   home.homeDirectory = "/Users/sadanand"; # REPLACE ME
   home.stateVersion = "23.05";
@@ -79,60 +82,6 @@
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
-  };
-
-  programs.vscode = {
-  enable = true;
-  extensions = with pkgs.vscode-extensions; [
-    astro-build.astro-vscode
-    bbenoist.Nix
-    DavidAnson.vscode-markdownlint
-    eamodio.gitlens
-    esbenp.prettier-vscode
-    formulahendry.auto-close-tag
-    GitHub.github-vscode-theme
-    GrapeCity.gc-excelviewer
-    Gruntfuggly.todo-tree
-    idleberg.applescript
-    James-Yu.latex-workshop
-    mechatroner.rainbow-csv
-    miguelsolorio.fluent-icons
-    monokai.theme-monokai-pro-vscode
-    moshfeu.compare-folders
-    ms-azuretools.vscode-docker
-    ms-python.black-formatter
-    ms-python.flake8
-    ms-python.isort
-    ms-python.python
-    ms-toolsai.jupyter
-    ms-toolsai.jupyter-keymap
-    ms-toolsai.jupyter-renderers
-    ms-toolsai.vscode-jupyter-cell-tags
-    ms-toolsai.vscode-jupyter-slideshow
-    ms-vscode-remote.remote-containers
-    ms-vscode-remote.remote-ssh
-    ms-vscode-remote.remote-ssh-edit
-    ms-vscode.cmake-tools
-    ms-vscode.cpptools
-    ms-vscode.cpptools-extension-pack
-    ms-vscode.cpptools-themes
-    ms-vscode.remote-explorer
-    njpwerner.autodocstring
-    redhat.vscode-commons
-    redhat.vscode-yaml
-    rioj7.command-variable
-    ronnidc.nunjucks
-    rust-lang.rust-analyzer
-    sleistner.vscode-fileutils
-    streetsidesoftware.code-spell-checker
-    tonka3000.raycast
-    twxs.cmake
-    unifiedjs.vscode-mdx
-    VisualStudioExptTeam.intellicode-api-usage-examples
-    VisualStudioExptTeam.vscodeintellicode
-    xaver.clang-format
-    yzhang.markdown-all-in-one
-    ];
   };
 
 }
