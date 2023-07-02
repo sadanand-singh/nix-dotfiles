5d96d160a3dde9939eaeb1b003f67d203cc972cf
Sadanand Singh <sadanand4singh@gmail.com>
2023-07-02 18:12:22 +0530

    extend gitconfig

--- home.nix
+++ home.nix
@@ -31,6 +31,35 @@
 
   programs.git = {
     enable = true;
+    userName = "Sadanand Singh";
+    userEmail = "sadanand4singh@gmail.com";
+    ignores = [ "*~" ".DS_Store" ".direnv" ".env" ".rgignore" ];
+    extraConfig = {
+      init = { defaultBranch = "main"; };
+      pull = { ff = "only"; };
+    };
+    delta = { enable = true; };
+    aliases = {
+      ci = "commit";
+      co = "checkout";
+      di = "diff";
+      dc = "diff --cached";
+      addp = "add -p";
+      shoe = "show";
+      st = "status";
+      unch = "checkout --";
+      br = "checkout";
+      bra = "branch -a";
+      newbr = "checkout -b";
+      rmbr = "branch -d";
+      mvbr = "branch -m";
+      cleanbr =
+        "!git remote prune origin && git co master && git branch --merged | grep -v '*' | xargs -n 1 git branch -d && git co -";
+      as = "update-index --assume-unchanged";
+      nas = "update-index --no-assume-unchanged";
+      al = "!git config --get-regexp 'alias.*' | colrm 1 6 | sed 's/[ ]/ = /'";
+      pub = "push -u origin HEAD";
+    };
     includes = [{ path = "~/.config/home-manager/gitconfig"; }];
   };
 
